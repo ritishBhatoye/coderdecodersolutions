f0b69b1b7f4d70a01333260f1903b54b44fdeb8b
Ritish Bhatoye <ritishrahul87@gmail.com>
2024-07-01 02:21:08 +0530

    Next jS

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "Coder Decoder Solution",
+  description: "Modern & Fast Developer ",
 };
 
 export default function RootLayout({
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/jsm-logo.png" sizes="any" />
+        <link rel="icon" href="/profile.png" sizes="any" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
