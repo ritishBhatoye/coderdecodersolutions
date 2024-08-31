e33745ccb7f9080dd977e58b6d94f301004aa604
Ritish Bhatoye <ritishrahul87@gmail.com>
2024-09-01 01:42:54 +0530

    Minor Fix RecentProject

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -59,7 +59,7 @@ const RecentProjects = () => {
                         key={index}
                         className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
                         style={{
-                          transform: translateX(-${5 * index + 2}px),
+                          transform: `translateX(-${5 * index + 2}px)`,
                         }}
                       >
                         <img src={icon} alt="icon5" className="p-2" />
